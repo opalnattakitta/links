b0dad180c0ab908b50269b433f4eba140bedfbe7
Opal <chuan255@newschool.edu>
2025-02-19 00:34:57 -0500

    Java add class text, link, audio

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,9 @@
 let channelBlocks = document.querySelector('#channel-blocks')
 let showVideoButton = document.querySelector('#show-video-button')
 let showImageButton = document.querySelector('#show-image-button')
+let showTextButton = document.querySelector('#show-text-button')
+let showLinkButton = document.querySelector('#show-link-button')
+let showAudioButton = document.querySelector('#show-audio-button')
 let showAllButton = document.querySelector('#show-all-button')
 
 
@@ -12,12 +15,24 @@ showVideoButton.onclick = () => {
 }
 
 showImageButton.onclick = () => {
-	// When I click show videos, it should add show-video clas
 	channelBlocks.classList.add('show-image')
 }
 
+showTextButton.onclick = () => {
+	channelBlocks.classList.add('show-text')
+}
+
+showLinkButton.onclick = () => {
+	channelBlocks.classList.add('show-link')
+}
+
+showAudioButton.onclick = () => {
+	channelBlocks.classList.add('show-audio')
+}
+
+
 showAllButton.onclick = () => {
 	// When I click show all, it should remove show-video class
-	channelBlocks.classList.remove('show-video', 'show-image')
+	channelBlocks.classList.remove('show-video', 'show-image', 'show-text', 'show-link', 'show-audio')
 }
 
